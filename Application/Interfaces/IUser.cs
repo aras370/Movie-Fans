96a2a0306720bc94f32742129ce2eb57b8148d73
aras370 <hesamehsani72@yahoo.com>
2023-12-29 01:04:45 +0330

    firstapp

---
+++ Application/Interfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer.Models;
+using DataLayer.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IUser
+    {
+        Task<User> Login(LoginViewModel login);
+
+        Task<bool>  IsExistUser(RegisterViewModel user);
+
+        Task AddUser(User user);
+    }
+}
---
+++ Application/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using DataLayer;
+using DataLayer.Models;
+using DataLayer.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class UserService : IUser
+    {
+
+        Context _context;
+
+        public UserService(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUser(User user)
+        {
+            _context.Add(user);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsExistUser(RegisterViewModel user)
+        {
+            return await _context.Users.AnyAsync(u => u.UserName == user.UserName);
+        }
+
+        public async Task<User> Login(LoginViewModel login)
+        {
+            return await _context.Users.Where(u => u.UserName == login.UserName && u.Password == login.Password)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
